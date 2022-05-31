4c4d80c51c504ed9dbae16b68de515813e928f47
Sefiyye <tu7l1lxat@code.edu.az>
2022-05-31 14:43:03 +0400

    task

---
+++ Pronia start/DAL/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pronia_start.Models;
+
+namespace Pronia_start.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Plant> Plants { get; set; }
+        public DbSet<PlantImage> PlantImages { get; set; }
+        public DbSet<Size> Sizes { get; set; }
+    }
+}
